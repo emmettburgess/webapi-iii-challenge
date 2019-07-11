33327c816866b154671d4e80768158e436226ce1
emmettburgess <47261227+emmettburgess@users.noreply.github.com>
2019-07-11 14:47:09 -0600

    postDb fixed

--- posts/postDb.js
+++ posts/postDb.js
@@ -1,4 +1,16 @@
-return db('posts')
+const db = require('../data/dbConfig.js');
+module.exports = {
+  get,
+  getById,
+  insert,
+  update,
+  remove,
+};
+function get() {
+  return db('posts');
+}
+function getById(id) {
+  return db('posts')
     .where({ id })
     .first();
 }
