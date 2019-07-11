e4cf577d434210f423c0b1ce1bef14b46845c629
emmettburgess <47261227+emmettburgess@users.noreply.github.com>
2019-07-11 14:44:43 -0600

    postDb updated

--- posts/postDb.js
+++ posts/postDb.js
@@ -1,23 +1,7 @@
-const db = require('../data/dbConfig.js');
-
-module.exports = {
-  get,
-  getById,
-  insert,
-  update,
-  remove,
-};
-
-function get() {
-  return db('posts');
-}
-
-function getById(id) {
-  return db('posts')
+return db('posts')
     .where({ id })
     .first();
 }
-
 function insert(post) {
   return db('posts')
     .insert(post)
@@ -25,13 +9,11 @@ function insert(post) {
       return getById(ids[0]);
     });
 }
-
 function update(id, changes) {
   return db('posts')
     .where({ id })
     .update(changes);
 }
-
 function remove(id) {
   return db('posts')
     .where('id', id)
